7f439b047a9f7ef29fa6d74e3ff5dcc75e81d2c7
Taeyang Jin (Theo) <sun@dataportal.kr>
2023-09-14 14:59:35 +0900

    add. 13-7

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <>Hello, Nextjs!</>;
+  return (
+    <>
+      Hello, Nextjs!
+      <br />
+      <img src="/next.svg" width="150px" />
+      <br />
+      <img src="/vercel.svg" width="150px" />
+    </>
+  );
 }
